23dba5cf8e02bed480956b7994e5ad72e380a4a3
Rémi Attab <remi.attab@gmail.com>
2014-05-14 22:28:33 -0400

    trie: first draft of node interface.

--- src/trie/node.h
+++ src/trie/node.h
@@ -14,4 +14,34 @@
 // node
 // -----------------------------------------------------------------------------
 
-struct trie_node;
+uint64_t trie_node_get(
+        struct ilka_region *r, ilka_ptr_t node,
+        struct ilka_key_it key);
+
+void trie_node_lb(
+        struct ilka_region *r, ilka_ptr_t node,
+        struct ilka_key_it key, struct ilka_key_it lb);
+
+void trie_node_ub(
+        struct ilka_region *r, ilka_ptr_t node,
+        struct ilka_key_it key, struct ilka_key_it ub);
+
+int trie_node_next(
+        struct ilka_region *r, ilka_ptr_t node,
+        struct ilka_key_it key, struct ilka_key_it prev);
+
+int trie_node_prev(
+        struct ilka_region *r, ilka_ptr_t node,
+        struct ilka_key_it key, struct ilka_key_it next);
+
+int trie_node_add(
+        struct ilka_region *r, ilka_ptr_t node,
+        struct ilka_key_it key, uint64_t value);
+
+int trie_node_cmp_xchg(
+        struct ilka_region *r, ilka_ptr_t node,
+        struct ilka_key_it key, uint64_t *expected, uint64_t desired);
+
+int trie_node_cmp_rmv(
+        struct ilka_region *r, ilka_ptr_t node,
+        struct ilka_key_it key, uint64_t *expected);
