3dcdb178401f76ce25f0287d45bdbc661656bdd0
Rémi Attab <remi.attab@gmail.com>
2015-09-20 16:13:55 -0400

    alloc: added mt tests.

--- tests/alloc_test.c
+++ tests/alloc_test.c
@@ -6,10 +6,12 @@
 #define ILKA_ABORT_ON_FAIL
 
 #include "check.h"
+#include "utils/log.h"
 #include "utils/arch.h"
 #include "utils/bits.h"
 #include "utils/rand.h"
 #include "utils/error.h"
+#include "utils/thread.h"
 #include "region/region.h"
 
 #include <stdlib.h>
@@ -51,34 +53,41 @@ inline void fill_block(
         }                                                               \
     } while(false)
 
-void run_alloc_test(
-        struct ilka_region *r, int id, size_t blocks, size_t allocs, size_t mul)
+struct alloc_test
 {
-    ilka_srand(id + 1);
-    struct alloc_node *nodes = calloc(blocks, sizeof(struct alloc_node));
+    struct ilka_region *r;
+    size_t blocks;
+    size_t allocs;
+    size_t mul;
+};
 
-    for (size_t alloc = 0; alloc < allocs; ++alloc) {
-        size_t i = ilka_rand_range(0, blocks);
+void run_alloc_test(size_t id, void *data)
+{
+    struct alloc_test *t = (struct alloc_test *) data;
+
+    ilka_srand(id + 1);
+    struct alloc_node *nodes = calloc(t->blocks, sizeof(struct alloc_node));
+
+    for (size_t alloc = 0; alloc < t->allocs; ++alloc) {
+        size_t i = ilka_rand_range(0, t->blocks);
 
         if (nodes[i].off) {
-            check_block(r, nodes[i].off, nodes[i].len);
-            ilka_free(r, nodes[i].off, nodes[i].len);
+            check_block(t->r, nodes[i].off, nodes[i].len);
+            ilka_free(t->r, nodes[i].off, nodes[i].len);
             nodes[i] = (struct alloc_node) {0};
         }
         else {
             size_t max = ilka_rand_range(2, 128);
-            nodes[i].len = ilka_rand_range(1, max) * mul;
-            nodes[i].off = ilka_alloc(r, nodes[i].len);
-            if (!nodes[i].off) ilka_abort();
-
-            fill_block(r, nodes[i].off, nodes[i].len, alloc);
+            nodes[i].len = ilka_rand_range(1, max) * t->mul;
+            nodes[i].off = ilka_alloc(t->r, nodes[i].len);
+            fill_block(t->r, nodes[i].off, nodes[i].len, alloc);
         }
     }
 
-    for (size_t i = 0; i < blocks; ++i) {
+    for (size_t i = 0; i < t->blocks; ++i) {
         if (!nodes[i].off) continue;
-        check_block(r, nodes[i].off, nodes[i].len);
-        ilka_free(r, nodes[i].off, nodes[i].len);
+        check_block(t->r, nodes[i].off, nodes[i].len);
+        ilka_free(t->r, nodes[i].off, nodes[i].len);
     }
 
     free(nodes);
@@ -94,7 +103,30 @@ START_TEST(page_test_st)
     struct ilka_options options = { .open = true, .create = true };
     struct ilka_region *r = ilka_open("blah", &options);
 
-    run_alloc_test(r, 0, 20, 20 * 100, ILKA_PAGE_SIZE);
+    struct alloc_test tdata = {
+        .r = r,
+        .blocks = 20,
+        .allocs = 20 * 100,
+        .mul = ILKA_PAGE_SIZE,
+    };
+    run_alloc_test(0, &tdata);
+
+    ilka_close(r);
+}
+END_TEST
+
+START_TEST(page_test_mt)
+{
+    struct ilka_options options = { .open = true, .create = true };
+    struct ilka_region *r = ilka_open("blah", &options);
+
+    struct alloc_test tdata = {
+        .r = r,
+        .blocks = 5,
+        .allocs = 5 * 100,
+        .mul = ILKA_PAGE_SIZE,
+    };
+    ilka_run_threads(run_alloc_test, &tdata);
 
     ilka_close(r);
 }
@@ -106,7 +138,31 @@ START_TEST(block_test_st)
     struct ilka_options options = { .open = true, .create = true };
     struct ilka_region *r = ilka_open("blah", &options);
 
-    run_alloc_test(r, 0, 100, 100 * 100, 1);
+    struct alloc_test tdata = {
+        .r = r,
+        .blocks = 100,
+        .allocs = 100 * 100,
+        .mul = 1,
+    };
+    run_alloc_test(0, &tdata);
+
+    ilka_close(r);
+}
+END_TEST
+
+
+START_TEST(block_test_mt)
+{
+    struct ilka_options options = { .open = true, .create = true };
+    struct ilka_region *r = ilka_open("blah", &options);
+
+    struct alloc_test tdata = {
+        .r = r,
+        .blocks = 100,
+        .allocs = 100 * 100,
+        .mul = 1,
+    };
+    ilka_run_threads(run_alloc_test, &tdata);
 
     ilka_close(r);
 }
@@ -120,7 +176,9 @@ END_TEST
 void make_suite(Suite *s)
 {
     ilka_tc(s, page_test_st, true);
+    ilka_tc(s, page_test_mt, true);
     ilka_tc(s, block_test_st, true);
+    ilka_tc(s, block_test_mt, true);
 }
 
 int main(void)
