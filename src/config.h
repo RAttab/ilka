b71207ab901a441f6a3fb29532a8a89cd3e0883b
Rémi Attab <remi.attab@gmail.com>
2015-11-23 11:18:22 -0500

    utils: updated config.h with mcheck.

--- src/config.h
+++ src/config.h
@@ -12,9 +12,11 @@
 
 /* #define ILKA_LOG */
 /* #define ILKA_LOG_RING 1 */
-/* #define ILKA_HASH_LOG */
-/* #define ILKA_HASH_BUCKET_LOG */
 
+/* #define ILKA_MCHECK 1 */
 /* #define ILKA_ALLOC_FILL_ON_FREE 1 */
 /* #define ILKA_ALLOC_FILL_ON_ALLOC 1 */
+
+/* #define ILKA_HASH_LOG */
+/* #define ILKA_HASH_BUCKET_LOG */
 
