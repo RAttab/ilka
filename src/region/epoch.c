c41c0d4de66f3d7824a6aea6ca650b9249f139b4
Rémi Attab <remi.attab@gmail.com>
2015-11-29 13:01:09 -0500

    epoch: formalized all the morders.

--- src/region/epoch.c
+++ src/region/epoch.c
@@ -172,17 +172,27 @@ static void epoch_defer_run(struct ilka_epoch *ep)
     ilka_assert(!slock_try_lock(&ep->lock), "lock is required for defer run");
     if (!ep->threads) return;
 
+    // morder_relaxed: doesn't synchronize with anyone since any increment is
+    // done while holding the lock that we're currently holding.
+    size_t current_epoch = ilka_atomic_load(&ep->epoch, morder_relaxed);
+
     struct epoch_thread *thread = ep->threads;
     while (thread) {
-        size_t epoch = ilka_atomic_load(&thread->epoch, morder_acquire);
-        if (epoch && epoch < ep->epoch) return;
+
+        //  morder_relaxed: only written when entering the region and there's
+        //  therefore no prior operations that we need to synchronize.
+        size_t epoch = ilka_atomic_load(&thread->epoch, morder_relaxed);
+        if (epoch && epoch < current_epoch) return;
         thread = thread->next;
     }
 
     thread = ep->threads;
-    size_t i = (ep->epoch - 1) % 2;
+    size_t i = (current_epoch - 1) % 2;
     while (thread) {
-        struct epoch_defer *defers = ilka_atomic_load(&thread->defers[i], morder_acquire);
+
+        // morder_consume: synchronizes epoch_defer_impl to ensure that all
+        // defer nodes have been fully written before we read it.
+        struct epoch_defer *defers = ilka_atomic_xchg(&thread->defers[i], 0, morder_consume);
 
         while (defers) {
             if (defers->fn) defers->fn(defers->data);
@@ -193,10 +203,17 @@ static void epoch_defer_run(struct ilka_epoch *ep)
             defers = next;
         }
 
-        ilka_atomic_store(&thread->defers[i], 0, morder_release);
+        // morder_relaxed: only epoch_world_stop requires that all defers be
+        // completed before proceeding but we do that syncrhonization through
+        // the lock.
+        ilka_atomic_store(&thread->defers[i], 0, morder_relaxed);
+
         thread = thread->next;
     }
 
+    // morder_release: synchronizes epoch_world_stop and ensures that all the
+    // defer lists have been fully cleared before allowing them to be filled up
+    // again.
     ilka_atomic_fetch_add(&ep->epoch, 1, morder_release);
 }
 
@@ -205,12 +222,18 @@ static bool epoch_defer_impl(struct ilka_epoch *ep, struct epoch_defer *node)
     struct epoch_thread *thread = epoch_thread_get(ep);
     if (!thread) return false;
 
-    size_t epoch = ilka_atomic_load(&ep->epoch, morder_acquire);
+    // morder_relaxed: pushing to a stale epoch is fine because it just means
+    // that our node is already obsolete and can therefore be executed
+    // right-away.
+    size_t epoch = ilka_atomic_load(&ep->epoch, morder_relaxed);
     struct epoch_defer **head = &thread->defers[epoch % 2];
 
-    struct epoch_defer *old_head = ilka_atomic_load(head, morder_relaxed);
+    struct epoch_defer *old_head = ilka_atomic_load(head, morder_acquire);
     do {
         node->next = old_head;
+
+        // morder_release: synchronizes with epoch_defer_run to ensure that our
+        // node has been fully written before it's read.
     } while (!ilka_atomic_cmp_xchg(head, &old_head, node, morder_release));
 
     return true;
@@ -287,17 +310,26 @@ static bool epoch_enter(struct ilka_epoch *ep)
         size_t epoch = ilka_atomic_load(&ep->epoch, morder_relaxed);
         ilka_atomic_store(&thread->epoch, epoch, morder_relaxed);
 
+        // morder_acq_rel: ensures that our thread is stamped with an epoch
+        // before we read world_lock. Otherwise, if the we check world_lock
+        // prior to stamping the epoch then it would be possible for world_lock
+        // to be set after we read it but before we stamp the epoch which means
+        // that we could enter the epoch after world_stop has returned to the
+        // user.
+        //
+        // We also require epoch_enter to be an acquire op so that no reads from
+        // within the region are hoisted out of the region.
         ilka_atomic_fence(morder_acq_rel);
 
         // it's possible for the global epoch to have switched between our load
         // and our store so make sure we have the latest version.
-        if (epoch != ilka_atomic_load(&ep->epoch, morder_relaxed))
+        if (ilka_unlikely(epoch != ilka_atomic_load(&ep->epoch, morder_relaxed)))
             ilka_atomic_store(&thread->epoch, 0, morder_relaxed);
 
         // the world lock is on so spin until we resume.
-        else if (ilka_atomic_load(&ep->world_lock, morder_relaxed)) {
+        else if (ilka_unlikely(ilka_atomic_load(&ep->world_lock, morder_acquire))) {
             ilka_atomic_store(&thread->epoch, 0, morder_relaxed);
-            while (ilka_atomic_load(&ep->world_lock, morder_relaxed));
+            while (ilka_atomic_load(&ep->world_lock, morder_acquire));
         }
 
         else return true;
@@ -312,6 +344,12 @@ static void epoch_exit(struct ilka_epoch *ep)
     ilka_assert(!!thread, "unexpected nil epoch thread");
     ilka_assert(thread->epoch, "exiting while not in epoch");
 
+    // morder_release: synchronizes with epoch_world_stop to ensure that all ops
+    // in the region are properly commited before indicating that the world has
+    // stopped.
+    //
+    // We also require that epoch_exit is an overall release op so that no reads
+    // from within the region are sunk below the region.
     ilka_atomic_store(&thread->epoch, 0, morder_release);
 
     // \todo: this is too aggressive. Should find a looser policy.
@@ -328,12 +366,16 @@ static void epoch_exit(struct ilka_epoch *ep)
 
 static void epoch_world_stop(struct ilka_epoch *ep)
 {
+    // morder_acquire: syncrhonizes with epoch_world_resume to ensure that all
+    // ops within the region stays within the region.
     ilka_atomic_fetch_add(&ep->world_lock, 1, morder_acquire);
     slock_lock(&ep->lock);
 
     struct epoch_thread *thread = ep->threads;
     while (thread) {
-        while (ilka_atomic_load(&thread->epoch, morder_relaxed));
+        // morder_acquire: synchronizes with epoch_exit to ensure that all ops
+        // within the enter/exit region are completed before we can continue.
+        while (ilka_atomic_load(&thread->epoch, morder_acquire));
         thread = thread->next;
     }
 
@@ -346,5 +388,7 @@ static void epoch_world_stop(struct ilka_epoch *ep)
 
 static void epoch_world_resume(struct ilka_epoch *ep)
 {
+    // morder_release: synchronizes with epoch_world_stop to ensure that all ops
+    // within the region stay within the region.
     ilka_atomic_fetch_add(&ep->world_lock, -1, morder_release);
 }
